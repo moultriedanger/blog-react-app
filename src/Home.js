3bf69e0bd8b2bcd6be41de527812d61549ae9212
Moultrie Dangerfield <moultriedanger@gmail.com>
2025-05-21 13:33:37 -0400

    added json server functionality

--- src/Home.js
+++ src/Home.js
@@ -2,28 +2,22 @@ import { useState, useEffect} from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ])
-
-  const [name, setName] = useState('mario');
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => (blog.id !== id));
-    setBlogs(newBlogs)
-  }
+  const [blogs, setBlogs] = useState(null)
 
   useEffect(() => {
-    console.log("use effect ran", name);
-  }, [blogs]);
+    fetch('http://localhost:8000/blogs') 
+     .then((response) => 
+        response.json()
+     )
+     .then((data) => {
+        console.log(data)
+        setBlogs(data)
+  })
+  }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title = "ALL BLOGS" handleDelete={handleDelete}/>
-      <button onClick={() => setName('luigi')}>Change Name</button>
-      <p>{name}</p>
+      {blogs && <BlogList blogs={blogs} title = "ALL BLOGS"/>}
     </div>
   );
 }
