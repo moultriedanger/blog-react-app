ddc3f3487f6939e8b798f9a97854d5d3706d0bb1
Moultrie Dangerfield <moultriedanger@gmail.com>
2025-05-21 10:44:37 -0400

    learned passing functions as props and useEffect hook

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,7 +1,5 @@
-const BlogList = (props) => {
-    const blogs = props.blogs;
-    const title = props.title;
-
+const BlogList = ({blogs, title, handleDelete}) => {
+    
     return(
         <div className="blog-list">
             <h2>{title}</h2>
@@ -9,8 +7,10 @@ const BlogList = (props) => {
                 <div className="blog-preview" key={blog.id} >
                 <h2>{ blog.title }</h2>
                 <p>Written by { blog.author }</p>
+                <button onClick={() => handleDelete(blog.id)}>Delete blog</button>
                 </div>
             ))}
+            
         </div>
     )
 }
--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect} from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -8,9 +8,22 @@ const Home = () => {
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ])
 
+  const [name, setName] = useState('mario');
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => (blog.id !== id));
+    setBlogs(newBlogs)
+  }
+
+  useEffect(() => {
+    console.log("use effect ran", name);
+  }, [blogs]);
+
   return (
     <div className="home">
-      <BlogList blogs={blogs} title = "ALL BLOGS"/>
+      <BlogList blogs={blogs} title = "ALL BLOGS" handleDelete={handleDelete}/>
+      <button onClick={() => setName('luigi')}>Change Name</button>
+      <p>{name}</p>
     </div>
   );
 }
